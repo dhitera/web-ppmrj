aeec235f8f1c7746b8c21f01fabc341c67de8d32
myusername <mradithyanaufal@gmail.com>
2024-10-22 02:34:17 +0700

    add template for announcement (still static)

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -1,5 +1,147 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Ini Halaman Pengumuman</h1>
+<div class="p-10">
+    <p class="text-2xl font-semibold text-center italic">Pengumuman Seleksi Gelombang 1</p>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-5">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3 bg-gray-300 dark:bg-gray-800 text-center">
+                        Nama Lengkap
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center bg-gray-50">
+                        Asal
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Muhammad Rifqi Ashari
+                    </th>
+                    <td class="px-6 py-4">
+                        Tangerang
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Aris Muhammad Rozaq
+                    </th>
+                    <td class="px-6 py-4">
+                        Sumedang
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Muhammad Ilman Nafian Alvalery
+                    </th>
+                    <td class="px-6 py-4">
+                        Cianjur
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Aswangga Pandita Sadu
+                    </th>
+                    <td class="px-6 py-4">
+                        Kediri
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Arga Ulya Abdurrohman
+                    </th>
+                    <td class="px-6 py-4">
+                        Karanganyar
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p class="text-2xl font-semibold text-center italic">Pengumuman Seleksi Gelombang 2</p>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-5">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3 bg-gray-300 dark:bg-gray-800 text-center">
+                        Nama Lengkap
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-center bg-gray-50">
+                        Asal
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Muhammad Rifqi Ashari
+                    </th>
+                    <td class="px-6 py-4">
+                        Tangerang
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Aris Muhammad Rozaq
+                    </th>
+                    <td class="px-6 py-4">
+                        Sumedang
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Muhammad Ilman Nafian Alvalery
+                    </th>
+                    <td class="px-6 py-4">
+                        Cianjur
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-300 dark:border-gray-700">
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Aswangga Pandita Sadu
+                    </th>
+                    <td class="px-6 py-4">
+                        Kediri
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-100 dark:text-white dark:bg-gray-800">
+                        Arga Ulya Abdurrohman
+                    </th>
+                    <td class="px-6 py-4">
+                        Karanganyar
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Informasi Tambahan:</h2>
+    <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li>
+            Bagi Pendaftar yang dinyatakan lulus tahap seleksi wajib untuk mengikuti kegiatan Orientasi Santri
+            (Informasi akan diberikan lebih lanjut).
+        </li>
+        <li>
+            Kudu Jama'ah Well
+        </li>
+    </ul>
+
+</div>
+
 @endsection
